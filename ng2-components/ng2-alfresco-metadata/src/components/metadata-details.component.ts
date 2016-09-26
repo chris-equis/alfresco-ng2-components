069a88418742a19850559a71b339a9ab30aada97
Cristi Socea <cristian.socea@ness.com>
2016-09-26 17:20:54 +0300

    Add metadata-details sub-component of metadata

---
+++ ng2-components/ng2-alfresco-metadata/src/components/metadata-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+declare let __moduleName: string;
+
+@Component({
+    moduleId: __moduleName,
+    selector: 'metadata-details',
+    templateUrl: './metadata-details.component.html'//,
+    // styleUrls: ['./metadata-details.component.css']
+})
+export class MetadataDetailsComponent {
+    @Input()
+    node: any;
+    // could import "MinimalNodeEntity" from "documentlist" component
+
+    constructor() {
+        console.log('constructed');
+    }
+}
