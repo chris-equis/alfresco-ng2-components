1e18af0f4a2dad3ba7a9a4a1692e7ab1258108bb
Bogdan Cilibiu <Bogdan.Cilibiu@ness.com>
2016-09-28 16:06:15 +0300

    edit properies dialog

--- ng2-components/ng2-alfresco-metadata/src/components/metadata-details.component.ts
+++ ng2-components/ng2-alfresco-metadata/src/components/metadata-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
 declare let __moduleName: string;
 
@@ -9,11 +9,31 @@ declare let __moduleName: string;
     styleUrls: ['./metadata-details.component.css']
 })
 export class MetadataDetailsComponent {
+    @ViewChild('dialog')
+    dialog: any;
+
     @Input()
     node: any;
     // could import "MinimalNodeEntity" from "documentlist" component
 
     constructor() {
         console.log('constructed');
+    }
+
+    public openDialog() {
+        if (this.dialog) {
+            this.dialog.nativeElement.showModal();
+        }
+    }
+
+    public save() {
+        alert('saved! :D');
+        this.cancel();
+    }
+
+     public cancel() {
+        if (this.dialog) {
+            this.dialog.nativeElement.close();
+        }
     }
 }
