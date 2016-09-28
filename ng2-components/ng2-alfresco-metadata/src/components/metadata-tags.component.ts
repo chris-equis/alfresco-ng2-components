a996f90e75a274f9563aebc1eab0fa760d419a6a
Cristi Socea <cristian.socea@ness.com>
2016-09-28 10:37:41 +0300

    Update on tags

--- ng2-components/ng2-alfresco-metadata/src/components/metadata-tags.component.ts
+++ ng2-components/ng2-alfresco-metadata/src/components/metadata-tags.component.ts
@@ -1,19 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MetadataService } from '../ng2-alfresco-metadata.service';
 
 declare let __moduleName: string;
 
 @Component({
     moduleId: __moduleName,
     selector: 'metadata-tags',
-    templateUrl: './metadata-tags.component.html'//,
-    // styleUrls: ['./metadata-tags.component.css']
+    templateUrl: './metadata-tags.component.html',
+    styleUrls: ['./metadata-tags.component.css'],
+    providers: [ MetadataService ]
 })
-export class MetadataTagsComponent {
+export class MetadataTagsComponent implements OnInit {
     @Input()
     node: any;
     // could import "MinimalNodeEntity" from "documentlist" component
 
-    constructor() {
-        console.log('constructed');
+    tags: string[] = [];
+    loading: boolean = false;
+    editing: boolean = false;
+
+    constructor(
+        private metadata: MetadataService
+    ) {
+        
+    }
+
+    get commaSeparatedTags() {
+        return this.tags.join(', ');
+    }
+
+    set commaSeparatedTags(value:string) {
+        this.tags = value.split(',')
+            .map((tag:string):string => tag.trim())
+            .filter((tag:string):boolean => !!tag);
+    }
+
+    ngOnInit() {
+        this.loading = true;
+
+        this
+            .metadata
+            .getNodeTags(this.node.id)
+            .finally(() => {
+                this.loading = false;
+            })
+            .subscribe((result:Object) => {
+                // this.tags = result.list.entries;
+                this.tags = ['Design', 'Flow chart', 'Activiti'];
+            }, error => {
+                this.tags = [];
+            })
     }
 }
