1a126a2bb829ad9631ecb51bd4d2b9545154376a
Cristi Socea <cristian.socea@ness.com>
2016-10-05 17:21:37 +0300

    Few more other changes

--- ng2-components/ng2-alfresco-metadata/src/components/metadata-components.ts
+++ ng2-components/ng2-alfresco-metadata/src/components/metadata-components.ts
@@ -2,3 +2,4 @@ export * from './metadata-advanced-properties/metadata-advanced-properties.compo
 export * from './metadata-comments/metadata-comments.component';
 export * from './metadata-details/metadata-details.component';
 export * from './metadata-tags/metadata-tags.component';
+export * from './metadata-edit-panel/metadata-edit-panel.component';
