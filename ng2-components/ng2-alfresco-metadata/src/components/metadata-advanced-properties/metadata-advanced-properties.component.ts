dcc07bdd7ae04bf8f3536230aa4c01ea8a809ecc
Cristi Socea <cristian.socea@ness.com>
2016-10-12 11:06:22 +0300

    MetadataAdvancedProperties uses MetadataAccordionComponent

--- ng2-components/ng2-alfresco-metadata/src/components/metadata-advanced-properties/metadata-advanced-properties.component.ts
+++ ng2-components/ng2-alfresco-metadata/src/components/metadata-advanced-properties/metadata-advanced-properties.component.ts
@@ -1,17 +1,51 @@
 import { Component, Input } from '@angular/core';
 
+import {
+  MetadataAccordionComponent
+} from './metadata-accordion/metadata-accordion.component';
+
+import {
+  MetadataAccordionItemComponent
+} from './metadata-accordion/metadata-accordion-item.component';
+
 declare let __moduleName: string;
 
 @Component({
     moduleId: __moduleName,
     selector: 'metadata-advanced-properties',
-    templateUrl: './metadata-advanced-properties.component.html'// ,
-    // styleUrls: ['./metadata-advanced-properties.component.css']
+    templateUrl: './metadata-advanced-properties.component.html',
+    // styleUrls: ['./metadata-advanced-properties.component.css'],
+    directives: [
+      MetadataAccordionComponent,
+      MetadataAccordionItemComponent
+    ]
 })
 export class MetadataAdvancedPropertiesComponent {
     @Input()
     node: any;
     // could import "MinimalNodeEntity" from "documentlist" component
+    
+    // Mock used for advanced properties
+    advancedPropertiesMock = [{
+      title: 'Dublin Core',
+      properties: [{
+        label: 'Publisher', value: ''
+      }, {
+        label: 'Contributor', value: ''
+      }, {
+        label: 'Type', value: ''
+      }]
+    }, {
+      title: 'Efectivity',
+      properties: [{
+        label: 'Property 1', value: 'Value 1'
+      }]
+    }, {
+      title: 'Geographic',
+      properties: [{
+        label: 'Property 2', value: ''
+      }]
+    }];
 
     constructor() {
     }
